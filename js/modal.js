c8364a0b8737b449de5f2d9c6f71f0ef105a6b7b
Kim Aleksandr <k1mal3xandre@yandex.ru>
2022-09-22 13:29:07 +0700

    modal

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector(".modal");
+const contactsBtn = document.querySelector(".contacts__btn");
+const closeBtn = document.querySelector(".modal__btn-close")
+
+contactsBtn.addEventListener("click", () => {
+  modal.classList.add("modal_active");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("modal_active")
+});
+
+// modal.addEventListener("click", (e) => {
+//   if (e.target === e.currentTarget) {
+//     modal.classList.remove("modal_active");
+//   }
+//   modal.removeEventListener("click", () => {
+//     modal.classList.remove("modal_active");
+//   });
+// });
